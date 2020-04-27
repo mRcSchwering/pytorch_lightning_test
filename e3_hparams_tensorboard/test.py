9bac3d50e2f62a4c9fc61ed961ca15957c8bd2af
Marc Schwering <marc.schwering@bayer.com>
2020-04-27 23:45:32 +0200

    todo: write own logger

---
+++ e3_hparams_tensorboard/test.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from torch.utils.tensorboard import SummaryWriter
+from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
+
+# TODO: use SummaryWriter directly
+# only change the way directories are named (or not? actually doesnt matter)
+# write logger from that...
+
+#this_dir = Path(__file__).parent.absolute()
+this_dir = Path('e3_hparams_tensorboard').absolute()
+
+# TODO: write logging class with this...
+with SummaryWriter(log_dir=this_dir / 'logs') as w:
+    for i in range(5):
+        w.add_scalar('y=2x', i * 2, i)
+    w.add_hparams({'lr': 0.1*i, 'bsize': i}, {'hparam/accuracy': 10*i, 'hparam/loss': 10*i})
+
+
+event_acc = EventAccumulator(str(this_dir / 'logs'))
+event_acc.Reload()
+print(event_acc.Tags())
+
+
+
+# E. g. get wall clock, number of steps and value for a scalar 'Accuracy'
+w_times, step_nums, vals = zip(*event_acc.Scalars('hparam/accuracy'))
+
+vals
