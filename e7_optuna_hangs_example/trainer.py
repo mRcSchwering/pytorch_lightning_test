3a7c4fcfbd7970e855ebe4c5108df5b327d48b92
Marc Schwering <marc.schwering@bayer.com>
2020-05-14 11:54:55 +0200

    fix

--- e7_optuna_hangs_example/trainer.py
+++ e7_optuna_hangs_example/trainer.py
@@ -4,92 +4,16 @@ Trying to find out why e6 is hanging on the last job.
 import os
 import threading
 from pathlib import Path
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import pytorch_lightning as pl
-from torch.optim import Adam
-from torch.utils.data import Dataset, DataLoader
 from src.loggers import HyperparamsSummaryTensorBoardLogger
 import optuna
 from src.multiproc import GpuQueue
-from e6_with_optuna.module import MyModule
+from e7_optuna_hangs_example.module import MyModule
 
 THIS_DIR = Path(__file__).parent.absolute()
 CUDA_VISIBLE_DEVICES = os.environ.get('CUDA_VISIBLE_DEVICES')
 N_GPUS = 0 if CUDA_VISIBLE_DEVICES is None else len(CUDA_VISIBLE_DEVICES.split(','))
 
-
-'''
-class RandomClassData(Dataset):
-    """Standard normal distributed features and uniformly sampled discrete targets"""
-
-    def __init__(self, n_samples: int, n_dim: int, n_classes: int = 2):
-        super(RandomClassData, self).__init__()
-        self.features = torch.rand((n_samples, n_dim))
-        self.targets = torch.randint(0, n_classes, size=(n_samples,))
-
-    def __len__(self):
-        return len(self.targets)
-
-    def __getitem__(self, i):
-        return self.features[i], self.targets[i]
-
-
-class TanhMlp(nn.Module):
-    """Multi layer perceptron with tanh activation function"""
-
-    def __init__(self, input_size: int, hidden_size: int, output_size: int, dropout_rate: float = .0):
-        super(TanhMlp, self).__init__()
-        self.hidden_fc = nn.Linear(input_size, hidden_size)
-        self.out_fc = nn.Linear(hidden_size, output_size)
-        self.dropout = nn.Dropout(dropout_rate)
-
-    def forward(self, x):
-        hidden = F.relu(self.hidden_fc(x))
-        out = torch.tanh(self.out_fc(self.dropout(hidden)))
-        return out
-
-
-class MyModule(pl.LightningModule):
-
-    def __init__(self, hparams: dict):
-        super(MyModule, self).__init__()
-        self.net = TanhMlp(10, hparams['hidden_size'], 2)
-        self.best_val_loss = 999.9
-        self.hparams = hparams
-
-    def forward(self, x):
-        return self.net(x)
-
-    def train_dataloader(self):
-        return DataLoader(dataset=RandomClassData(200, 10), batch_size=self.hparams['batch_size'])
-    
-    def val_dataloader(self):
-        return DataLoader(dataset=RandomClassData(100, 10), batch_size=self.hparams['batch_size'])
-
-    def configure_optimizers(self):
-        return Adam(self.parameters(), lr=1e-3)
-
-    def training_step(self, batch, batch_idx: int) -> dict:
-        return self._forward_pass(*batch)
-
-    def validation_step(self, batch, batch_idx: int) -> dict:
-        return self._forward_pass(*batch)
-
-    def validation_epoch_end(self, steps):
-        targets = torch.cat([d['targets'] for d in steps], dim=0)
-        loss = float((torch.stack([d['loss'] for d in steps]).sum() / len(targets)).cpu().numpy())
-        if loss < self.best_val_loss:
-            self.best_val_loss = loss
-        log = {'best_val_loss': self.best_val_loss}
-        return {'loss': loss, 'log': log}
-
-    def _forward_pass(self, x, y) -> float:
-        preds = self.forward(x)
-        loss = F.cross_entropy(preds, y)
-        return {'loss': loss, 'preds': preds, 'targets': y}
-'''
 
 def train_with_params(trial_config, trial_i, gpu_i):
     print(f'Starting trial {trial_i} pid:{os.getpid()} tid:{threading.get_ident()}')
@@ -112,7 +36,7 @@ def train_with_params(trial_config, trial_i, gpu_i):
         num_sanity_val_steps=0,
         progress_bar_refresh_rate=0)
 
-    model = MyModule(hparams, metrics={})
+    model = MyModule(hparams)
     trainer.fit(model)
     print(f'Finished trial {trial_i} pid:{os.getpid()} tid:{threading.get_ident()}')
     return model.best_val_loss
