65dfe743f5ba19df48a664195e6d2ec8c59d1425
Marc Schwering <marc.schwering@bayer.com>
2020-05-14 11:58:05 +0200

    fix

--- e7_optuna_hangs_example/module.py
+++ e7_optuna_hangs_example/module.py
@@ -4,36 +4,8 @@ import torch.nn.functional as F
 import pytorch_lightning as pl
 from torch.optim import Adam
 from torch.utils.data import Dataset, DataLoader
-
-
-class RandomClassData(Dataset):
-    """Standard normal distributed features and uniformly sampled discrete targets"""
-
-    def __init__(self, n_samples: int, n_dim: int, n_classes: int = 2):
-        super(RandomClassData, self).__init__()
-        self.features = torch.rand((n_samples, n_dim))
-        self.targets = torch.randint(0, n_classes, size=(n_samples,))
-
-    def __len__(self):
-        return len(self.targets)
-
-    def __getitem__(self, i):
-        return self.features[i], self.targets[i]
-
-
-class TanhMlp(nn.Module):
-    """Multi layer perceptron with tanh activation function"""
-
-    def __init__(self, input_size: int, hidden_size: int, output_size: int, dropout_rate: float = .0):
-        super(TanhMlp, self).__init__()
-        self.hidden_fc = nn.Linear(input_size, hidden_size)
-        self.out_fc = nn.Linear(hidden_size, output_size)
-        self.dropout = nn.Dropout(dropout_rate)
-
-    def forward(self, x):
-        hidden = F.relu(self.hidden_fc(x))
-        out = torch.tanh(self.out_fc(self.dropout(hidden)))
-        return out
+from src.dataloading import RandomClassData
+from src.modeling import TanhMlp
 
 
 class MyModule(pl.LightningModule):
